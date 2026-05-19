be6e057b89ab3336270477d8e8e62bdf8243b246
AIZOFan <als.irfan28@gmail.com>
2026-05-19 09:52:16 +0700

    full update

--- Fasum/fasum/lib/main.dart
+++ Fasum/fasum/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fasum/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const HomeScreen();
+            return const SplashScreen();
           } else {
             return const SignInScreen();
           }
